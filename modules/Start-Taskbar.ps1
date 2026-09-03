2f4a74e6fa3d38d00462ceb9e8c3c0cae6dfef33
cristian082 <147616312+cristian082@users.noreply.github.com>
2026-09-03 22:33:53 +0200

    Add safe Start and taskbar usability tweaks

--- modules/Start-Taskbar.ps1
+++ modules/Start-Taskbar.ps1
@@ -19,4 +19,14 @@ function Invoke-TDTStartTaskbar {
     if ($Config.DisableSearchHighlights -and $PSCmdlet.ShouldProcess('SearchSettings', 'Disabilitare evidenziazioni ricerca')) {
         Set-TDTUserDword -RelativePath 'Software\Microsoft\Windows\CurrentVersion\SearchSettings' -Name 'IsDynamicSearchBoxEnabled' -Value 0 -AllUsers $allUsers
     }
+
+    # Removes web suggestions from the Windows search box without disabling Windows Search/indexing.
+    if ($Config.DisableWebSearchSuggestions -and $PSCmdlet.ShouldProcess('Windows Search', 'Disabilitare suggerimenti web nella casella di ricerca')) {
+        Set-TDTUserDword -RelativePath 'Software\Policies\Microsoft\Windows\Explorer' -Name 'DisableSearchBoxSuggestions' -Value 1 -AllUsers $allUsers
+    }
+
+    # Windows 11: enables the useful "End task" command on taskbar app context menus.
+    if ($Config.EnableTaskbarEndTask -and $PSCmdlet.ShouldProcess('Taskbar', 'Abilitare Termina attivita dal menu della barra')) {
+        Set-TDTUserDword -RelativePath 'Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings' -Name 'TaskbarEndTask' -Value 1 -AllUsers $allUsers
+    }
 }
